e4c88fc3025268036d91cca9e27e74fd88e5e070
alzaburetz <anton.motin.dev@gmail.com>
2020-04-08 18:02:30 +0300

    Your location page added

---
+++ MarketPlace/MarketPlace/Views/YourLocation.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MarketPlace.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class YourLocation : ContentPage
+    {
+        public YourLocation()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMainApp(object sender, EventArgs args)
+        {
+            Application.Current.MainPage = new MainPage();
+        }
+
+        private void FocusField(object sende, EventArgs args)
+        {
+            Town.Focus();
+        }
+    }
+}
