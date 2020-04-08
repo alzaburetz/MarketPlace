bb5fa363f5e0247dc6739916393d2aa0ff419077
alzaburetz <anton.motin.dev@gmail.com>
2020-04-08 18:00:57 +0300

    Filled enty with background color added

---
+++ MarketPlace/MarketPlace.Android/Renderers/FilledEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+using MarketPlace.Droid.Renderers;
+using MarketPlace.Controls;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(FilledEntry), typeof(FilledEntryRenderer))]
+namespace MarketPlace.Droid.Renderers
+{
+    public class FilledEntryRenderer:EntryRenderer
+    {
+        public FilledEntryRenderer(Context context) : base(context) { }
+        Color Background { get; set; }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                Background = (e.NewElement as FilledEntry).Background;
+                if (this.Background == null)
+                {
+                    Background = Color.FromHex("#F4F4F4");
+                }
+                Control.SetBackgroundColor(this.Background.ToAndroid());
+                Control.SetPadding(40, 40, 40, 40);
+            }
+        }
+    }
+}
