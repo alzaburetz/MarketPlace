cb830625375f8cf3c271316dd958967ba644181b
alzaburetz <anton.motin.dev@gmail.com>
2020-04-09 15:53:52 +0300

    Page for category added

---
+++ MarketPlace/MarketPlace/Views/CategoryPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MarketPlace.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoryPage : ContentPage
+    {
+        public CategoryPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoToProduct(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new ProductPage());
+        }
+    }
+}
