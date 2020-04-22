b2245742c741bccb6c76a210900c16edde92676f
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 22:59:39 +0300

    Generic data model added

---
+++ MarketPlace/MarketPlace/Models/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class Data
+    {
+        [JsonProperty("cats")]
+        public List<Category> Categories { get; set; }
+    }
+}
