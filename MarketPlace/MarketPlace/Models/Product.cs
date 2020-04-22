70ed13aff4108caddccaf880ad30023a162cc39b
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 23:00:10 +0300

    Product model added

---
+++ MarketPlace/MarketPlace/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class Product
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("img")]
+        public List<ImageObject> Images { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("price")]
+        public int Price { get; set; }
+    }
+}
