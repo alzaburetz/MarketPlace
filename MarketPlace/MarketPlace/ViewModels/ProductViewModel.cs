4ec2c8ee2019391faa5e992f29de2d1cccc2d7a9
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 23:02:15 +0300

    ViewModel for product page added, loading of one product

---
+++ MarketPlace/MarketPlace/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MarketPlace.Models;
+
+namespace MarketPlace.ViewModels
+{
+    public class ProductViewModel:BaseViewModel
+    {
+        Product product;
+        public Product Product
+        {
+            get => product;
+            set
+            {
+                product = value;
+                OnPropertyChanged("Product");
+            }
+        }
+
+        public ProductViewModel(Product prod)
+        {
+            Product = prod;
+        }
+    }
+}
