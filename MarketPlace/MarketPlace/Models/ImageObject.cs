e82d10db2713838b3299ee519c4c49fbaf77db0b
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 22:59:53 +0300

    Image model added

---
+++ MarketPlace/MarketPlace/Models/ImageObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class ImageObject
+    {
+        [JsonProperty("target_id")]
+        public int ID { get; set; }
+        [JsonProperty("url")]
+        public string URL { get; set; }
+    }
+}
