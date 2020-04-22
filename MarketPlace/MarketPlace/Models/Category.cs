fcee8fcfa7067fa5ae8987db0877edf62ff63fe0
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 22:59:21 +0300

    Category model added

---
+++ MarketPlace/MarketPlace/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class Category
+    {
+        [JsonProperty("tid")]
+        public int ID { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("uri")]
+        public string URI { get; set; }
+        [JsonProperty("parent")]
+        public int Parent { get; set; }
+        [JsonProperty("img_url")]
+        public string Img { get; set; }
+        [JsonIgnore]
+        public List<Category> Subcategories { get; set; }
+    }
+}
