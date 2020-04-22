fb789bc08959a29ee8adac555ddd6f9fc34f6abb
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-22 23:01:20 +0300

    ViewModel for categories page added, loading of categories and subcategories

---
+++ MarketPlace/MarketPlace/ViewModels/CategoryPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using MarketPlace.Models;
+using Xamarin.Forms;
+
+using System.Threading.Tasks;
+
+namespace MarketPlace.ViewModels
+{
+    public class CategoryPageViewModel:BaseViewModel
+    {
+        public ObservableCollection<Category> Categories { get; set; }
+        public List<Category> Cats { get; set; }
+        public Command LoadCategoriesCommand { get; set; }
+        public Command OpenCategory { get; set; }
+        public CategoryPageViewModel()
+        {
+            Categories = new ObservableCollection<Category>();
+            LoadCategoriesCommand = new Command(() =>
+            {
+                Task.Run(async () =>
+                {
+                    IsBusy = true;
+                    Categories.Clear();
+                    Cats = await Http.GetRequest<List<Category>>("getdata?action=getcat", false);
+                    if (Cats != null)
+                    {
+                        foreach (var category in Cats)
+                        {
+                            var cat = category;
+                            cat.Subcategories = Cats.FindAll(x => x.Parent == category.ID);
+                            if (cat.Parent == 0)
+                            Device.BeginInvokeOnMainThread(() => Categories.Add(cat));
+                        }
+                    }
+                    IsBusy = false;
+                });
+            });
+
+            OpenCategory = new Command<int>((id) =>
+            {
+
+            });
+        }
+    }
+}
