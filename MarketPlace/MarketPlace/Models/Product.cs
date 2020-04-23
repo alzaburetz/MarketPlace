76d459345fbe98fa627dc2bafac10a8d6ecfc32c
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 18:38:41 +0300

    Model optimized for database and Favorite field added

--- MarketPlace/MarketPlace/Models/Product.cs
+++ MarketPlace/MarketPlace/Models/Product.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 
 using Newtonsoft.Json;
+using LiteDB;
 
 namespace MarketPlace.Models
 {
     [Serializable]
-    public class Product
+    public class Product: MarketPlace.ViewModels.BaseViewModel
     {
+        [LiteDB.BsonId]
+        [JsonIgnore]
+        public virtual int id { get => ID; set => ID = value; }
         [JsonProperty("id")]
+        [BsonField("ID")]
         public int ID { get; set; }
         [JsonProperty("name")]
         public string Name { get; set; }
@@ -19,5 +24,18 @@ namespace MarketPlace.Models
         public string Description { get; set; }
         [JsonProperty("price")]
         public int Price { get; set; }
+
+        [JsonIgnore]
+        public bool Favorited
+        {
+            get => _favorited;
+            set
+            {
+                _favorited = value;
+                OnPropertyChanged("Favorited");
+            }
+        }
+        [JsonIgnore]
+        private bool _favorited;
     }
 }
