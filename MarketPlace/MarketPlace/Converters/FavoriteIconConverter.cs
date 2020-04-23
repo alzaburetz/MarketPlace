64a30c3e515a14d639b86be095060fea5b3dc34e
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 18:38:03 +0300

    Favorite icon converter added, returns string of icon source

---
+++ MarketPlace/MarketPlace/Converters/FavoriteIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MarketPlace.Converters
+{
+    public class FavoriteIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool favorited = (bool)value;
+            if (favorited)
+            {
+                return "favorited";
+            }
+            else
+            {
+                return "favorite";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
