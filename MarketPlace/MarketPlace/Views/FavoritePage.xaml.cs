19a61c8c4cf6cff30650fe04dcfb6b1fc44988cb
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 19:22:45 +0300

    Data binding is set, Page is subscribed to message to load favorite products

--- MarketPlace/MarketPlace/Views/FavoritePage.xaml.cs
+++ MarketPlace/MarketPlace/Views/FavoritePage.xaml.cs
@@ -7,14 +7,19 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MarketPlace.ViewModels;
+
 namespace MarketPlace.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FavoritePage : ContentPage
     {
+        FavoriteViewModel viewModel { get; set; }
         public FavoritePage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new FavoriteViewModel();
+            MessagingCenter.Subscribe<MainPage>(this, "LoadFavorite",(sender) => viewModel.LoadFavorite.Execute(null));
         }
     }
 }
