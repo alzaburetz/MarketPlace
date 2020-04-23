eb5627a0807b21e26fc0eaa5287fa6d040895c71
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 19:18:26 +0300

    Favorite viewmodel added, loading data from database

---
+++ MarketPlace/MarketPlace/ViewModels/FavoriteViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+using MarketPlace.Models;
+using System.Threading.Tasks;
+
+using System.Linq;
+
+namespace MarketPlace.ViewModels
+{
+    public class FavoriteViewModel:BaseViewModel
+    {
+        public ObservableCollection<Product> FavoriteProducts { get; set; }
+        public Command LoadFavorite { get; set; }
+        public Command RemoveFromFavorite { get; set; }
+        public FavoriteViewModel()
+        {
+            FavoriteProducts = new ObservableCollection<Product>();
+            LoadFavorite = new Command(() =>
+            {
+                IsBusy = true;
+                Task.Run(async () =>
+                {
+                    FavoriteProducts.Clear();
+                    var favorite = (await Database.GetAllStreamAsync<Product>("Favorite")).GetEnumerator();
+                    while (favorite.MoveNext())
+                    {
+                        Device.BeginInvokeOnMainThread(() => FavoriteProducts.Add(favorite.Current));
+                    }
+                    IsBusy = false;
+                });
+            });
+
+            RemoveFromFavorite = new Command<Product>((product) =>
+            {
+                FavoriteProducts.Remove(product);
+                Task.Run(() =>
+                {
+                    Database.RemoveItem<Product>("Favorite", LiteDB.Query.Where("_id", x => x.AsInt32 == product.ID));
+                    MessagingCenter.Send<FavoriteViewModel, Product>(this, "UnsetFavorite", product);
+                });
+            });
+        }
+    }
+}
