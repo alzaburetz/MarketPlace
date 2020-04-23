cdfe9a373a6183582411754d29369525cdf2f590
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 19:15:13 +0300

    Cart viewmodel added, Loading data from database and adding it to collection

---
+++ MarketPlace/MarketPlace/ViewModels/CartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using MarketPlace.Models;
+using Xamarin.Forms;
+
+namespace MarketPlace.ViewModels
+{
+    public class CartViewModel : BaseViewModel
+    {
+        public Command LoadCart { get; set; }
+        public ObservableCollection<CartItem> CartItems { get; set;}
+        public CartViewModel()
+        {
+            CartItems = new ObservableCollection<CartItem>();
+            LoadCart = new Command(async () =>
+            {
+                CartItems.Clear();
+                var cart = (await Database.GetAllStreamAsync<CartItem>("Cart")).GetEnumerator();
+                while (cart.MoveNext())
+                {
+                    CartItems.Add(cart.Current);
+                }
+            });
+        }
+    }
+}
