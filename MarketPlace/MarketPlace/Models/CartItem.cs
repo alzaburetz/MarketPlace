5bf2634a443b4564da6e247d57f3eca9712b4d09
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 18:38:51 +0300

    Cart Item added

---
+++ MarketPlace/MarketPlace/Models/CartItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using LiteDB;
+
+namespace MarketPlace.Models
+{
+    public class CartItem:Product
+    {
+        [BsonId]
+        public override int id { get; set; }
+        public int Count { get; set; }
+
+        public CartItem() { }
+        public CartItem(Product product)
+        {
+            Name = product.Name;
+            Favorited = product.Favorited;
+            Count = 1;
+            Description = product.Description;
+            Price = product.Price;
+            Images = product.Images;
+            id = product.ID;
+        }
+    }
+}
