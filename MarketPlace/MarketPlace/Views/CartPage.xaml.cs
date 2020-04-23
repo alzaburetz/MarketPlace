7f28107fb9e18f6e0a08b961c08e686d12d1fff8
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-23 18:54:21 +0300

    Binding to viewModel added, Page is subscribed to message to load cart

--- MarketPlace/MarketPlace/Views/CartPage.xaml.cs
+++ MarketPlace/MarketPlace/Views/CartPage.xaml.cs
@@ -7,14 +7,19 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MarketPlace.ViewModels;
+
 namespace MarketPlace.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CartPage : ContentPage
     {
+        CartViewModel viewModel { get; set; }
         public CartPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new CartViewModel();
+            MessagingCenter.Subscribe<MainPage>(this, "LoadCart", (s) => viewModel.LoadCart.Execute(null));
         }
     }
 }
