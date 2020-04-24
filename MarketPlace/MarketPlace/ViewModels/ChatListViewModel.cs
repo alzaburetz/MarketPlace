7b5c5cfa8cd6e594e77065bd793e1e41697f5d68
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:02:13 +0300

    Command to get chatlist added

--- MarketPlace/MarketPlace/ViewModels/ChatListViewModel.cs
+++ MarketPlace/MarketPlace/ViewModels/ChatListViewModel.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Text;
 
+using Xamarin.Forms;
+using MarketPlace.Models;
+using System.Threading.Tasks;
+
 namespace MarketPlace.ViewModels
 {
-    class ChatListViewModel
+    public class ChatListViewModel:BaseViewModel
     {
+        public Command LoadChat { get; set; }
+        public ObservableCollection<ChatData> ChatList { get; set; }
+        int UserID;
+        public ChatListViewModel()
+        {
+            ChatList = new ObservableCollection<ChatData>();
+            LoadChat = new Command(() =>
+            {
+                IsBusy = true;
+                Task.Run(async () =>
+                {
+                    ChatList.Clear();
+                    var chat_list = await Http.GetRequest<List<ChatData>>("chat?action=list", true);
+                    if (chat_list != null)
+                    {
+                        foreach (var element in chat_list)
+                        {
+                            Device.BeginInvokeOnMainThread(() => ChatList.Add(element));
+                        }
+                    }
+                    IsBusy = false;
+                });
+            });
+            MessagingCenter.Subscribe<MainPageViewModel, int>(this, "SetID", (s, id) => UserID = id);
+        }
     }
 }
