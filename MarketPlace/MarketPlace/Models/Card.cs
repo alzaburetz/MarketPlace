98ed1c62af3251171e794f6544a70fb175209510
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 17:59:21 +0300

    Card model added

---
+++ MarketPlace/MarketPlace/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class Card:ModelChanged
+    {
+        private string mask;
+        [JsonProperty("mask")]
+        public string Mask
+        {
+            get => mask;
+            set
+            {
+                mask = value;
+                OnPropertyChanged("Mask");
+            }
+        }
+    }
+}
