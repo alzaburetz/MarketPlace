9dd6370e1b67860a41ba5c4783fd7a634269aef4
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 17:59:40 +0300

    File model added

---
+++ MarketPlace/MarketPlace/Models/Files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class Files
+    {
+        public int target_id { get; set; }
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("alt")]
+        public string Alt { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+}
