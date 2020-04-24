d529321d79f85be16757f4284d2b892327f6406c
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:00:01 +0300

    ModelChanged class added

---
+++ MarketPlace/MarketPlace/Models/ModelChanged.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace MarketPlace.Models
+{
+    public class ModelChanged:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
