fc51b40a2f561a00dece4985e5861934bda2eb57
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:03:13 +0300

    MainPage viewmodel added, Loading user and passing id to chat screen, if user is empty, MainPage of application is Authorisation Page

---
+++ MarketPlace/MarketPlace/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using MarketPlace.Models;
+
+namespace MarketPlace.ViewModels
+{
+    public class MainPageViewModel:BaseViewModel
+    {
+        public Command LoadUser { get; set; }
+        public MainPageViewModel()
+        {
+            LoadUser = new Command(async () =>
+            {
+                var user = await Http.GetRequest<UserData2>("user", true);
+                if (user != null)
+                {
+                    MessagingCenter.Send<MainPageViewModel, int>(this, "SetID", user.user.uid);
+                }
+            });
+        }
+    }
+}
