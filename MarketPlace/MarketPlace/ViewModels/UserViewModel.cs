3c834396c373eb25b2003078cc93da35468264e6
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:03:30 +0300

    User viewModel added to load profile data

---
+++ MarketPlace/MarketPlace/ViewModels/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MarketPlace.Models;
+
+namespace MarketPlace.ViewModels
+{
+    public class UserViewModel:BaseViewModel
+    {
+        public Command LoadUser { get; set; }
+        public Command Updateuser { get; set; }
+        public UserData User { get; set; }
+        public UserViewModel()
+        {
+            LoadUser = new Command(() =>
+            {
+                Task.Run(async () =>
+                {
+                    var resp = await Http.GetRequest<UserData>("user", true);
+                    if (resp != null)
+                    {
+                        Device.BeginInvokeOnMainThread(() => User = resp);
+                    }
+                });
+            });
+            Updateuser = new Command<UserInfo>((user) =>
+            {
+                Task.Run(async () =>
+                {
+                    await Http.PostRequest<UserInfo>("user/update", user, true);
+                });
+            });
+        }
+    }
+}
