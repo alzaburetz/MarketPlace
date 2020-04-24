26bde85687cf3203dcd1dcc1a4f11a4a802949ff
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:05:19 +0300

    ViewModel binding context added

--- MarketPlace/MarketPlace/Views/AuthorizationPage.xaml.cs
+++ MarketPlace/MarketPlace/Views/AuthorizationPage.xaml.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MarketPlace.ViewModels;
+
 namespace MarketPlace.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AuthorizationPage : ContentPage
     {
+        AuthorizationViewModel viewModel { get; set; }
         public AuthorizationPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new AuthorizationViewModel();
         }
 
         protected async void Continue(object sender, EventArgs args)
