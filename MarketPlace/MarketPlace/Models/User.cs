03f274e5e3f9f1748c69a073d969896419af8246
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-24 18:00:11 +0300

    User Model added

---
+++ MarketPlace/MarketPlace/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketPlace.Models
+{
+    [Serializable]
+    public class User
+    {
+        public string phone;
+        public string code;
+
+        public User(string phone = "", string code = "")
+        {
+            this.phone = phone;
+            this.code = code;
+        }
+    }
+}
