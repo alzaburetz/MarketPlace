64366e6e7deaf89d0a1d1983e3c45100f70eda6a
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-27 17:53:45 +0300

    Converter that returns White is count == 0, Red otherwise

---
+++ MarketPlace/MarketPlace/Converters/BadgeBackgroundConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MarketPlace.Converters
+{
+    public class BadgeBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value == 0)
+            {
+                return Color.White;
+            }
+            else
+            {
+                return Color.Red;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
