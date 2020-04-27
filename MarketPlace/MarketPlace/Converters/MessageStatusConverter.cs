ed11fe13e66f54639357711560736d17b671d2e7
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-27 17:54:22 +0300

    Image resource converter for message status

---
+++ MarketPlace/MarketPlace/Converters/MessageStatusConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MarketPlace.Converters
+{
+    public class MessageStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (int)value;
+            if (status == 0)
+            {
+                return "sent";
+            }    
+            else
+            {
+                return "seen";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
