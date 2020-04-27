897685853a656c69ada9d2249eec50f8a9d7018d
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-27 17:54:51 +0300

    Converter for timestamp to time added

---
+++ MarketPlace/MarketPlace/Converters/TimeStampToTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MarketPlace.Converters
+{
+    public class TimeStampToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long timestamp = (long)value;
+            return DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime().ToString("HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
