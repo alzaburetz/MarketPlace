d4393b40bfde55377501f5ceb81eaf04a4bd8958
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-27 18:38:28 +0300

    Page methods implemented. When message collection changed, scrolls to bottom

--- MarketPlace/MarketPlace/Views/Messages.xaml.cs
+++ MarketPlace/MarketPlace/Views/Messages.xaml.cs
@@ -7,14 +7,46 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MarketPlace.ViewModels;
+using MarketPlace.Models;
+
 namespace MarketPlace.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Messages : ContentPage
     {
-        public Messages()
+        MessagesViewModel viewModel { get; set; }
+        public Messages(int reciever, int sender, UserInfo user, string title)
         {
             InitializeComponent();
+            BindingContext = viewModel = new MessagesViewModel(sender, reciever, user, title);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.LoadChat.Execute(null);
+            Collection.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "ItemsSource")
+                {
+                    viewModel.Messages.CollectionChanged += (newitems, olditems) =>
+                    {
+                        (s as CollectionView).ScrollTo(viewModel.Messages.Count);
+                    };
+                }
+            };
+        }
+
+        private void ClearChat(object sender, EventArgs args)
+        {
+            Message.Text = string.Empty;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            viewModel.CancellationSource.Cancel();
         }
     }
 }
