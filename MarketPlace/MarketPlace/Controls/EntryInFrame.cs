b6c40b27f76335693f20eaf5b009b6aa384ede93
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-27 17:53:16 +0300

    EntryInFrame control added

---
+++ MarketPlace/MarketPlace/Controls/EntryInFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MarketPlace.Controls
+{
+    public class EntryInFrame:Entry
+    {
+        public Color BorderColor { get; set; }
+        public int Padding { get; set; }
+        public int CornerRadius { get; set; }
+        public EntryInFrame()
+        {
+            if (BorderColor == null)
+            {
+                BorderColor = Color.FromHex("#A9A8A8");
+            }    
+        }
+    }
+}
