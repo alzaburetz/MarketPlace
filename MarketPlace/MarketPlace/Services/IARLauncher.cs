9ee5ec6201cb57f1d0d73b8273228f4af2af7ae9
Anton Motin <anton.motin.dev@gmail.com>
2020-05-05 18:55:56 +0300

    Interface for launching AR screen added

---
+++ MarketPlace/MarketPlace/Services/IARLauncher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketPlace.Services
+{
+    public interface IARLauncher
+    {
+        void LaunchAR();
+    }
+}
