f7fded8742cf20629c8f9b4fd1f7724492714bad
Anton Motin <anton.motin.dev@gmail.com>
2020-05-05 18:56:59 +0300

    Opening AR Activity implemented on Android

---
+++ MarketPlace/MarketPlace.Android/ARImpl/ARMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Google.AR.Core;
+
+using Xamarin.Forms;
+using MarketPlace.Services;
+using MarketPlace.Droid.ARImpl;
+using MarketPlace.Droid;
+
+
+using Plugin.CurrentActivity;
+
+[assembly:Dependency(typeof(ARMain))]
+namespace MarketPlace.Droid.ARImpl
+{
+    public class ARMain : IARLauncher
+    {
+        Session session { get; set; }
+        public void LaunchAR()
+        {
+            var activity = CrossCurrentActivity.Current.Activity;
+            var intent = new Intent(activity, typeof(ARActivity));
+            activity.StartActivity(intent);
+        }
+    }
+}
